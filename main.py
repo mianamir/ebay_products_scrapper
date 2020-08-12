f64e7dd74ef59fd46ad55c8f97a317b467d5aec6
mianamir <mianamirlahore@gmail.com>
2020-08-12 20:14:58 +0300

    adding more functions for Ebay products scrapping

--- main.py
+++ main.py
@@ -63,10 +63,36 @@ def write_csv(data, url):
         writer.writerow(row)
 
 
-def main():
-    url = 'https://www.ebay.com/sch/i.html?_nkw=mens+watches&_pgn=1'
+def get_simple_products_url():
+    # For customized URL mapping
+    keyword = "bgs 9.5 zion williamson silver prizm -draft -emergent -select -mosaic -fireworks -duke -crusade -instant -american"
+    number = 1
+    # old_url = 'https://www.ebay.com/sch/i.html?_nkw=mens+watches&_pgn=1'
+    base_url = 'https://www.ebay.com/sch/i.html?'
+    request_keyword = f"_nkw={keyword}"
+    page_number = f"&_pgn={str(number)}"
+    url = f"{base_url}{request_keyword}{page_number}"
 
-    products = get_index_data(get_page(url))
+    return url
+
+
+def get_sold_products_url():
+    # For customized URL mapping
+    keyword = "psa 10 zion williamson silver prizm -draft -emergent -select -mosaic -fireworks -duke -crusade -instant -american"
+    number = 1
+    # old_url = 'https://www.ebay.com/sch/i.html?_nkw=mens+watches&_pgn=1'
+    base_url = 'https://www.ebay.com/sch/i.html?'
+    request_keyword = f"_nkw={keyword}"
+    page_number = f"&_pgn={str(number)}"
+    sold_item_filter = f"&_sacat=0&rt=nc&LH_Sold=1&LH_Complete=1&_pgn=2"
+    url = f"{base_url}{request_keyword}{sold_item_filter}{page_number}"
+
+    return url
+
+
+def main():
+
+    products = get_index_data(get_page(get_simple_products_url()))
 
     for link in products:
         data = get_detail_data(get_page(link))
